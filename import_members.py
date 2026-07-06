2dc9f89f4052341b5fe737574277819598feb192
Adieltheanalyst <adielngugi@gmail.com>
2026-07-06 16:44:26 +0300

    new commit

--- import_members.py
+++ import_members.py
@@ -3,3 +3,7 @@ BuildIT Connective - Bulk Member Import
 Running it once to import existing sheet members and email them 
 
 """
+
+import sys , os ,secrets ,sqlite3, hashlib,time, random
+sys.path.insert(0, os.path.dirname(__file__))
+from email_utils import send_welcome_email, ZOHO_EMAIL, SITE_URL
